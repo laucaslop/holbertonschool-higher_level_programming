5643bc5be291fe0555679f385cf2e5b7fa88108a
laucaslop <1495@holbertonschool.com>
2020-09-01 21:17:55 +0000

    add

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 5:
+	print("{:d} and is greater than 5".format(number))
+elif number == 0:
+	print("{:d} and is 0".format(number))
+else:
+	print("{:d} and is less than 6 and not 0".format(number))

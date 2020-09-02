f661440eb2b53d77bba0d5503734650899321e9a
laucaslop <1495@holbertonschool.com>
2020-09-02 05:51:37 +0000

    delete whitespace

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for a in range(0, 9):
     for b in range(a + 1, 10):
         if a == 8 and b == 9:
-            print('{:d}{:d}'.format(a, b), sep='')
+            print('{:d}{:d}'.format(a,b), sep='')
         else:
             print("{:d}{:d}".format(a,b), end=', ')

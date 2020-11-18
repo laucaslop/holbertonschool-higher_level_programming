8c1ec865313f67b66f502a22adfbe4e3ba2dbc48
laucaslop <1495@holbertonschool.com>
2020-11-18 03:18:48 +0000

    add

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- script that creates a database and a table into database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);

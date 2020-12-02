a9e02425fe97dc63def4273b285780bd183dad9e
laucaslop <1495@holbertonschool.com>
2020-12-02 14:07:07 +0000

    add finall

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module """
+
+
+class Student:
+    """ Class Student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initial """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary """
+
+        new_dict = {}
+        if type(attrs) is list and all(type(i) is str for i in attrs):
+            for att in attrs:
+                if att in self.__dict__:
+                    new_dict[att] = self.__dict__[att]
+            return new_dict
+        return self.__dict__
+
